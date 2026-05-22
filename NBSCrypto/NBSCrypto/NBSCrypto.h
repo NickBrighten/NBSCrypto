f88df5d6b03cab98612b790f23fe8b2f2bb40243
Nick Brighten <96994740+NickBrighten@users.noreply.github.com>
2026-05-22 12:23:13 +0200

    Add files via upload

---
+++ NBSCrypto/NBSCrypto/NBSCrypto.h
@@ -0,0 +1,18 @@
+//
+//  NBSCrypto.h
+//  NBSCrypto
+//
+//  Created by NB Systems on 22.05.26.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NBSCrypto.
+FOUNDATION_EXPORT double NBSCryptoVersionNumber;
+
+//! Project version string for NBSCrypto.
+FOUNDATION_EXPORT const unsigned char NBSCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NBSCrypto/PublicHeader.h>
+
+
