522b09bd135167550362f9d031226003185a5e78
Nick Brighten <nick.brighten@nb-systems.com>
2026-07-07 17:08:21 +0200

    Optimising code and structure

--- NBSCrypto/NBSCrypto/classes/src/headers/nbs_mac.h
+++ NBSCrypto/NBSCrypto/classes/src/headers/nbs_mac.h
@@ -13,20 +13,22 @@
 #pragma mark - MAC STRUCTS
 
 typedef struct {
-    int cipher, block_len, buflen, keylen;
+    int block_len, buflen, keylen;
     unsigned char akey[MAXBLOCKSIZE], ACC[MAXBLOCKSIZE], IV[MAXBLOCKSIZE];
+    unsigned long cipher;
     cipher_state cs;
 } f9_state;
 
 typedef struct {
-    int hash;
     unsigned char key[MAXBLOCKSIZE];
+    unsigned long hash;
     hash_state hs;
 } hmac_state;
 
 typedef struct {
-    int cipher, block_len, buflen;
+    int block_len, buflen;
     unsigned char block[MAXBLOCKSIZE], prev[MAXBLOCKSIZE], Lu[2][MAXBLOCKSIZE];
+    unsigned long cipher;
     cipher_state cs;
 } omac_state;
 
@@ -37,9 +39,10 @@ typedef struct {
 } pelican_state;
 
 typedef struct {
-    int cipher, block_len, buflen;
+    int block_len, buflen;
     unsigned char Ls[32][MAXBLOCKSIZE], Li[MAXBLOCKSIZE], Lr[MAXBLOCKSIZE], block[MAXBLOCKSIZE], checksum[MAXBLOCKSIZE];
     unsigned long block_index;
+    unsigned long cipher;
     cipher_state cs;
 } pmac_state;
 
@@ -52,22 +55,22 @@ typedef struct {
 
 
 
-#pragma mark - MAC FUNCTIONS -
+#pragma mark - MAC FUNCTIONS
 
 #pragma mark F9
-int f9_init(int cipher, const unsigned char *key, unsigned long keylen, f9_state *f9);
+int f9_init(unsigned long cipher, const unsigned char *key, unsigned long keylen, f9_state *f9);
 int f9_process(const unsigned char *in, unsigned long inlen, f9_state *f9);
 int f9_done(unsigned char *out, unsigned long *outlen, f9_state *f9);
 
 
 #pragma mark HMAC
-int hmac_init(int hash, const unsigned char *key, unsigned long keylen, hmac_state *hmac);
+int hmac_init(unsigned long hash, const unsigned char *key, unsigned long keylen, hmac_state *hmac);
 int hmac_process(const unsigned char *in, unsigned long inlen, hmac_state *hmac);
 int hmac_done(unsigned char *out, unsigned long *outlen, hmac_state *hmac);
 
 
 #pragma mark OMAC
-int omac_init(int cipher, const unsigned char *key, unsigned long keylen, omac_state *omac);
+int omac_init(unsigned long cipher, const unsigned char *key, unsigned long keylen, omac_state *omac);
 int omac_process(const unsigned char *in, unsigned long inlen, omac_state *omac);
 int omac_done(unsigned char *out, unsigned long *outlen, omac_state *omac);
 
@@ -79,7 +82,7 @@ int pelican_done(unsigned char *out, pelican_state *pelmac);
 
 
 #pragma mark PMAC
-int pmac_init(int cipher, const unsigned char *key, unsigned long keylen, pmac_state *pmac);
+int pmac_init(unsigned long cipher, const unsigned char *key, unsigned long keylen, pmac_state *pmac);
 int pmac_process(const unsigned char *in, unsigned long inlen, pmac_state *pmac);
 int pmac_done(unsigned char *out, unsigned long *outlen, pmac_state *pmac);
 
