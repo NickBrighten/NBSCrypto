7f88dd406a260d7cb1f88b86190ec05fe2f2de01
Nick Brighten <nick.brighten@nb-systems.com>
2026-08-11 17:03:10 +0200

    Add support for 128-Bit, 160-Bit, 192-Bit, 224-Bit and 256-Bit

--- NBSCrypto/NBSCrypto/classes/src/modes/gcm-siv.c
+++ NBSCrypto/NBSCrypto/classes/src/modes/gcm-siv.c
@@ -294,8 +294,12 @@ int gcm_siv_encrypt_decrypt(int cipher, const unsigned char *key, unsigned long 
 
     if ((err = is_cipher_valid(cipher)) != NBSCrypto_OK) return err;
     if (cipher_descriptor[cipher].block_length != 16)	return NBSCrypto_ERROR;
-    if (keylen != 16 && keylen !=24 && keylen != 32)	return NBSCrypto_ERROR;
-    if (noncelen != 12)					return NBSCrypto_ERROR;
+    if (keylen != 16 &&
+	keylen != 20 &&
+	keylen != 24 &&
+	keylen != 28 &&
+	keylen != 32)	return NBSCrypto_ERROR;
+    if (noncelen != 12)	return NBSCrypto_ERROR;
     if (*taglen < 16) {
 	*taglen = 16;
 	return NBSCrypto_ERROR;
